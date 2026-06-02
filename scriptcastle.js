b00fed45b82a84a6ddbc543b57a5a1f2ceaf5672
farumarov03 <57535747+farumarov03@users.noreply.github.com>
2026-06-02 20:38:46 +0500

    Add getRndVal function to retrieve min and max values

--- scriptcastle.js
+++ scriptcastle.js
@@ -9,6 +9,7 @@ function randomNumber(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 function getRndVal() {
     min = document.getElementById("min").value;
     max = document.getElementById("max").value;
